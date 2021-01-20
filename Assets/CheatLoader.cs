cd2a1aa3fb9fb01f17bdb470eb31f9d3cb04278d
mrdivdiz <divdizcraft@gmail.com>
2021-01-20 15:30:48 +0400

    0.8
    0.8 update

---
+++ Assets/CheatLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatLoader : MonoBehaviour {
+
+	// Use this for initialization
+	public void Loadin () {
+		Application.LoadLevel(1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
